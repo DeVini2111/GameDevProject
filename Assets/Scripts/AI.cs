e836e41e4557a361739a1a57a28593f8935941d0
Adrien Bellot <adribellot@live.be>
2023-12-04 21:01:35 +0200

    AI v2
    AI can now use all units and some units have more chances to spawn than others

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -9,6 +9,8 @@ public class AI : MonoBehaviour
 {
     public GameObject warrior; //this means 1 variable by units... not good, TO CHANGE
     public GameObject archer;
+    public GameObject wizard;
+    public GameObject king;
     public Transform spawnPoint;
     public GameManager gameManager;
     public GameManager.Player Player2;
@@ -35,18 +37,34 @@ public class AI : MonoBehaviour
     //add to redo the sale code for test purposes but bad, TO CHANGE
     public void SpawnUnit()
     {
-        unitspawn = Random.Range(1, 3);
-        if (Time.time > nextSpawnTime && unitspawn == 1 && moneyRef_P2.GetComponent<moneyCountP2>().money >= warrior.GetComponent<Warrior>().GetCost())
+        //unitspawn = Random.Range(1, 11); debug only
+        print(unitspawn);
+        if (Time.time > nextSpawnTime && unitspawn < 3 && moneyRef_P2.GetComponent<moneyCountP2>().money >= warrior.GetComponent<Warrior>().GetCost())
         {
             Instantiate(warrior, spawnPoint.position, spawnPoint.rotation);
             gameManager.AddUnit(warrior.GetComponent<Warrior>(), Player2);
             nextSpawnTime = Time.time + cooldownTime;
         }
-        else if(Time.time > nextSpawnTime && unitspawn == 2 && moneyRef_P2.GetComponent<moneyCountP2>().money >= archer.GetComponent<Archer>().GetCost())
+        else if(Time.time > nextSpawnTime && (unitspawn >= 4 && unitspawn <= 6) && moneyRef_P2.GetComponent<moneyCountP2>().money >= archer.GetComponent<Archer>().GetCost())
         {
             Instantiate(archer, spawnPoint.position, spawnPoint.rotation);
             gameManager.AddUnit(archer.GetComponent<Archer>(), Player2);
             nextSpawnTime = Time.time + cooldownTime;
         }
+
+        if (Time.time > nextSpawnTime && (unitspawn > 6 && unitspawn <= 8) && moneyRef_P2.GetComponent<moneyCountP2>().money >= wizard.GetComponent<Wizard>().GetCost())
+        {
+            Instantiate(wizard, spawnPoint.position, spawnPoint.rotation);
+            gameManager.AddUnit(wizard.GetComponent<Wizard>(), Player2);
+            nextSpawnTime = Time.time + cooldownTime;
+        }
+        else if (Time.time > nextSpawnTime && (unitspawn >= 9 && unitspawn <= 10) && moneyRef_P2.GetComponent<moneyCountP2>().money >= king.GetComponent<King>().GetCost())
+        {
+            Instantiate(king, spawnPoint.position, spawnPoint.rotation);
+            gameManager.AddUnit(king.GetComponent<King>(), Player2);
+            nextSpawnTime = Time.time + cooldownTime;
+        }
+
+
     }
 }
